b33120aeaa876c2b0ba7544c559d7ecd3e165386
maxiaoan <maxiaoansuccess@qq.com>
2019-12-25 15:34:03 +0800

    Update superMall

---
+++ supermall/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configWebpack: {
+  		resolve: {
+  		alias: {
+  			'assets': '@/assets',
+  			'common': '@/common',
+  			'components': '@/components',
+  			'network': '@/network',
+  			'views': '@/views'
+  		 }
+  		}
+  	}
+  }
